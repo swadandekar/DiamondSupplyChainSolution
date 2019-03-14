fe96d66acc6d51aeb9517015af7b9a09c441cabb
swadandekar <42243658+swadandekar@users.noreply.github.com>
2019-03-14 08:57:28 -0400

    updated truffle js for rinkeby deployment

--- truffle.js
+++ truffle.js
@@ -1,9 +1,29 @@
+var HDWalletProvider = require('truffle-hdwallet-provider');
+
+var mnemonic = 'either worry increase noise toddler bamboo essay patch person defense lizard auction';
+
 module.exports = {
-  networks: {
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 200
+    }
+  },
+  networks: { 
     development: {
-      host: "127.0.0.1",
+      host: '127.0.0.1',
       port: 9545,
-      network_id: "*" // Match any network id
+      network_id: "*"
+    }, 
+    rinkeby: {
+      provider: function() { 
+        return new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io/v3/6b0d542a89d04e62b4b1d7a59ee86f58') 
+      },
+      port:8545,
+      network_id: 4,
+      gas: 4500000,
+      gasPrice: 10000000000,
+
     }
   }
 };
